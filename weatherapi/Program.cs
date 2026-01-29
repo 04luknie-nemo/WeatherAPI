30c3451a101ee5e45f4d40ef6877c8aa37f107f9
Lukas <04luknie@skola.boras.se>
2026-01-29 10:05:36 +0100

    Base structure

---
+++ weatherapi/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Lägg till den DbContext ni skapar för er databas här
+// builder.Services.AddDbContext<ApiDbContext>(options =>
+//    options.UseInMemoryDatabase("EventDb"));
+
+//Låt detta vara kvar! Utan denna inställning kommer inte websidan att få access till API:et.
+// Läs mer här: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Denna hör ihop med CORS-inställningen ovan
+app.UseCors();
+
+//Ni ska inte skriva era endpoints här i Program.cs utan i separata controllers, så använd denna:
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+var positivesums = new[]
+{
+    "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+var negativesums = new[]
+{
+    "Frysande", "Bracing", "Chilly", "Cool"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
